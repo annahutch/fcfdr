186ee15cf1dcc783cb1c9e2ebb735a22f1c8e7d4
Anna Hutchinson <annahutchinson1995@gmail.com>
2020-12-03 12:25:27 +0000

    update binary cFDR function

--- R/binary_cfdr.R
+++ R/binary_cfdr.R
@@ -3,11 +3,14 @@
 #' @param p p values for principal trait (vector of length n)
 #' @param q binary auxillary data values (vector of length n)
 #' @param chr chromosome each SNP resides (vector of length n)
+#' @param p.thr p-value threshold for interpolation based on digits_highp and digits_lowp
+#' @param digits_highp digits for rounding for p >= p.thr
+#' @param digits_lowp digits for rounding for p < p.thr
 #'
 #' @return dataframe of p, q and v values
 #' @export
 #'
-binary_cfdr <- function(p, q, chr){
+binary_cfdr <- function(p, q, chr, p.thr = 0.01, digits_highp = 1, digits_lowp = 2){
   
   unique_chr <- unique(chr)
   
@@ -19,42 +22,60 @@ binary_cfdr <- function(p, q, chr){
   v_res <- vector(mode = "list", length = length(unique_chr))
   
   for(j in 1:length(unique_chr)){
-    
     chrom <- unique_chr[j]
-    
     p_loo <- p[-which(chr == chrom)] # df leave-one-chromosome-out
     q_loo <- q[-which(chr == chrom)] # df leave-one-chromosome-out
-    ps <- p_res[[j]]
-    qs <- q_res[[j]]
+    ps <- c(p_res[[j]], 1, 1) # add p = 1 to prevent interpolating for p>ps[use]
+    qs <- c(q_res[[j]], 0, 1)
+    
+    ## sample values to fit the function
+    
+    # sample less in p > p.thr, more in p < p.thr
+    lps = ifelse(ps<p.thr, round(log(ps), digits_lowp), round(log(ps), digits_highp))
+    use = c(ifelse(qs[-c(length(ps)-1, length(ps))]==0, !duplicated(lps * (qs==0)), !duplicated(lps * (qs!=0))), TRUE, TRUE) # add p = 1 to prevent interpolating for p>ps[use]
     
     q0 <- length(which(q_loo == 1 & p_loo > 0.5))/length(which(p_loo > 0.5))
-    
     mult <- (length(which(q_loo == 0 & p_loo > 1/2))/length(which(q_loo == 1 & p_loo > 1/2)))
     
-    sol <- ifelse(qs==0, 
-                  mult*ps / (max(1, length(which(p_loo <=  ps & q_loo == 0)))),
-                  (1/mult)*ps / (max(1, length(which(p_loo <=  ps & q_loo == 1)))))
+    q0_sol=sapply(ps, function(p) max(sum(p_loo <= p & q_loo==0),1))
+    q1_sol=sapply(ps, function(p) max(sum(p_loo <= p & q_loo==1),1))
     
-    f <- function(p, q, c) (p/max(1,length(which(p_loo <=  ps & q_loo == q)))) - c
+    sol <- ifelse(qs==0,
+                  mult*ps / q0_sol,
+                  (1/mult)*ps / q1_sol)
     
+    sol.use=sol[use]
+    
+    f <- function(p, q, c) (p/max(1,length(which(p_loo <=  p & q_loo == q)))) - c
+    tols=ifelse(ps<1e-4,ps,.Machine$double.eps^0.25)
     p1 <- ps
-    for(i in which(qs==0)){
-      p1[i] <- ifelse(ps[i] < 1e-04,
-                      min(1,uniroot(f, c(0, 1), q = 1, c = sol[i], extendInt = "yes", tol = ps[i], maxiter = 5000)$root),
-                      min(1,uniroot(f, c(0, 1), q = 1, c = sol[i], extendInt = "yes", maxiter = 5000)$root))
+    for(i in which(qs==0 & use)){
+      p1[i] <- min(1,uniroot(f, c(0,1), q = 1, c = sol[i], extendInt = "yes",
+                             tol = tols[i],
+                             maxiter = 5000)$root)
     }
-    
     p0 <- ps
     for(i in which(qs==1)){
-      p0[i] <- ifelse(ps[i] < 1e-04,
-                      min(1,uniroot(f, c(0, 1), q = 0, c = sol[i], extendInt = "yes", tol = ps[i], maxiter = 5000)$root),
-                      min(1,uniroot(f, c(0, 1), q = 0, c = sol[i], extendInt = "yes", maxiter = 5000)$root))
+      p0[i] <- min(1,uniroot(f, c(0,1), q = 0, c = sol[i], extendInt = "yes",
+                             tol = tols[i],
+                             maxiter = 5000)$root)
     }
     
-    v_res[[j]] <- p0*(1-q0) + p1*q0
     
+    f = approxfun(ps[use & qs==0],p1[use & qs==0])
+    p1[!use & qs==0] = f(ps[!use & qs==0])
+    f = approxfun(ps[use & qs==1],p0[use & qs==1])
+    p0[!use & qs==1] = f(ps[!use & qs==1])
+    
+    v_res[[j]] <- p0*(1-q0) + p1*q0
+    # remove the v-values for the artificially added p=1 points
+    v_res[[j]] <- v_res[[j]][-c(length(ps)-1, length(ps))]
+    
+    ## fix kinks - fails - need to depend on qs too
+    # ord=order(v_res[[j]])
+    # revord=numeric(length(ord))
+    # revord[ord]=seq_along(ord)
+    # v_res[[j]] = cummax(v_res[[j]][ord])[revord]
   }
-  
   data.frame(p = unsplit(p_res, f = chr), q = unsplit(q_res, f = chr), v = unlist(v_res))
-  
 }
