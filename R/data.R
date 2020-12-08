78a23fda0ef83b1ad93dc59ad18dd67aa14e6e16
Anna Hutchinson <annahutchinson1995@gmail.com>
2020-12-08 10:48:19 +0000

    roxygen2 documentation for the package data objects

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Dataframe for T1D analysis
+#'
+#' data.frame object containing columns for chromosome, position (GRCh37),
+#' rsid, effect allele, reference allele, T1D p-value from Onengut-Gumuscu et al. (2015),
+#' binary DGF annotation, continuous H3K27ac fold enrichment value in T helper cells, RA p-value 
+#' from Eyre et al. (2012) and LDAK weight for each SNP (row)
+#' 
+#'
+#' @format a data frame
+"df"
