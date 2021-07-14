05ac72de89d2ebd00e87da705a3837a35a8a29c9
Anna Hutchinson <annahutchinson1995@gmail.com>
2021-07-14 17:37:08 +0100

    .

--- R/flexible_cfdr.R
+++ R/flexible_cfdr.R
@@ -87,12 +87,11 @@ flexible_cfdr <- function(p, q, indep_index, res_p = 300, res_q = 500, nxbin = 1
     warning=function(cond) {
       message("Warning from locfdr:")
       message(cond)
-      message("Try running flexible_cfdr again and adjusting locfdr_df parameter")
+      message("Examine the fit to the data")
+      message("See locfdr documentation here: https://cran.r-project.org/web/packages/locfdr/vignettes/locfdr-example.pdf")
       message("...")
-      message("To determine optimal value for locfdr_df parameter (df in locfdr::locfdr) see locfdr documentation here: ")
-      message("https://cran.r-project.org/web/packages/locfdr/vignettes/locfdr-example.pdf")
-      message("The fcfdr::zz_in_locfdr function can be used to output the zz vector")
-      message("that flexible_cfdr inputs into locfdr internally")
+      message("Alternatively, the fcfdr::parameters_in_locfdr function can be used to output the parameters used as input in locfdr::locfdr")
+      locfdr(c(zp_ind, -zp_ind), bre = c(kpq$x[-length(kpq$x)] + diff(kpq$x)/2, kpq$x[length(kpq$x)] + diff(kpq$x)[length(diff(kpq$x))]/2, -c(kpq$x[-length(kpq$x)] + diff(kpq$x)/2, kpq$x[length(kpq$x)] + diff(kpq$x)[length(diff(kpq$x))]/2)), mlests = c(mlests[1], b*mlests[2]), plot = 0, df = locfdr_df)
     }
   )
   
